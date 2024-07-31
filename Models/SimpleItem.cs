85e1d90b534f2e96b2661948384dbf1b5a8762e6
Taina Jesse <jesse.taina@hiq.fi>
2024-07-31 13:14:30 +0300

    formatting

--- Models/SimpleItem.cs
+++ Models/SimpleItem.cs
@@ -3,20 +3,20 @@ using Newtonsoft.Json;
 namespace Models
 {
 
-public class SimpleItem
-{
-    [JsonProperty("id")]
-    public Guid Id { get; set; }
-    [JsonProperty("name")]
-    public string? Name { get; set; }
-    [JsonProperty("description")]
-    public string? Description { get; set; }
-    [JsonProperty("price")]
-    public decimal Price { get; set; }
-    [JsonProperty("available")]
-    public bool Available { get; set; }
-    [JsonProperty("timestamp")]
-    public DateTime Timestamp { get; set; }
-}
+    public class SimpleItem
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+        [JsonProperty("description")]
+        public string? Description { get; set; }
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+        [JsonProperty("available")]
+        public bool Available { get; set; }
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+    }
 
 }
